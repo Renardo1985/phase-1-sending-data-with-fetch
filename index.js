43952df530037cb21401c57ae665a7407950b3cc
Renardo Williams <rennwill@gmail.com>
2023-04-17 15:07:08 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+   
+submitData("Renardo","ren@gmail.com")  
+
+function submitData(user, email){
+    const userData = { name: user, email: email}
+    const config = {
+        method: "POST",
+        headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+        },
+        body: JSON.stringify(userData),
+    }
+    return fetch("http://localhost:3000/users", config)
+        .then(function(res){return res.json()})
+        .then(function(object){
+            console.log(object);
+            document.querySelector('body').innerHTML = object.id;
+        })
+        .catch(function (error) {document.querySelector('body').innerHTML = error.message});        
+}
